6f8e282dce3410b1f5bcaff31c64f5e0d39ec406
Asdren Xhafa <asdrenxhafa@gmail.com>
2021-02-19 16:34:25 +0100

    restaurantmenu listing and data to json

---
+++ front/src/components/RestaurantItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-md-6 col-sm-6">
+    <h4>{{ this.restaurant.name }}</h4><span><img :src="this.restaurant.logo"></span>
+  </div>
+</template>
+
+<script>
+export default {
+name: "RestaurantItem",
+  props : ['restaurant']
+}
+</script>
+
+<style scoped>
+
+</style>
--- front/src/components/RestaurantsMenu.vue
+++ front/src/components/RestaurantsMenu.vue
@@ -7,38 +7,33 @@
           <h1 class="heading">Places to eat...</h1>
           <hr>
         </div>
-        <div class="col-md-6 col-sm-6">
-          <h4>Hamburger ABA</h4><span><img src="images/abas.jpg"></span>
 
-        </div>
-        <div class="col-md-6 col-sm-6">
-          <h4>Green and Protein</h4><span><img src="images/green.jpg"></span>
+        <RestaurantItem v-for="restaurant in restaurants" :key="restaurant.id" :restaurant="restaurant"></RestaurantItem>
 
-        </div>
-        <div class="col-md-6 col-sm-6">
-          <h4>Papillon</h4><span><img src="images/papillon.jpg"></span>
 
-        </div>
-        <div class="col-md-6 col-sm-6">
-          <h4>Papillon</h4><span><img src="images/papillon.jpg"></span>
-
-        </div>
-        <div class="col-md-6 col-sm-6">
-          <h4>Papillon</h4><span><img src="images/papillon.jpg"></span>
-
-        </div>
-        <div class="col-md-6 col-sm-6">
-          <h4>Papillon</h4><span><img src="images/papillon.jpg"></span>
-
-        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import RestaurantItem from "@/components/RestaurantItem";
 export default {
-name: "RestaurantsMenu"
+name: "RestaurantsMenu",
+  components: {RestaurantItem},
+  data(){
+    return{
+      restaurants :
+          [
+            {logo : 'images/papillon.jpg' , name : 'Papillon'},
+            {logo : 'images/papillon.jpg' , name : 'Papillon'},
+            {logo : 'images/papillon.jpg' , name : 'Papillon'},
+            {logo : 'images/papillon.jpg' , name : 'Papillon'},
+            {logo : 'images/papillon.jpg' , name : 'Papillon'},
+            {logo : 'images/papillon.jpg' , name : 'Papillon'},
+          ]
+    }
+  },
 }
 </script>
 
