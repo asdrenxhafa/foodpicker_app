2a7310167261176cf60a61a95de4fdb5159401cd
Blerina Maxharraj <bm45555@ubt-uni.net>
2021-03-10 16:51:38 +0100

    Login Page

--- html_front/js/main.js
+++ html_front/js/main.js
@@ -63,6 +63,21 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
+     /*==================================================================
+    [ Show pass ]*/
+    var showPass = 0;
+    $('.btn-show-pass').on('click', function(){
+        if(showPass == 0) {
+            $(this).next('input').attr('type','text');
+            $(this).addClass('active');
+            showPass = 1;
+        }
+        else {
+            $(this).next('input').attr('type','password');
+            $(this).removeClass('active');
+            showPass = 0;
+        }
+        
+    });
 
 })(jQuery);
