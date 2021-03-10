fe6428422ea311533938f99e95dfda3d7f5c9a82
Leutrim Ahmeti <trimmahmeti@outlook.com>
2021-03-10 18:06:44 +0100

    delete method for restaurants

--- front/src/components/admin/Restaurants/Restaurants.vue
+++ front/src/components/admin/Restaurants/Restaurants.vue
@@ -1,9 +1,9 @@
 <template>
   <v-simple-table>
+    <v-btn slot="activator" color="primary" dark class="mb-2">New Item</v-btn>
     <template v-slot:default>
       <thead>
       <h1>Restaurants</h1>
-
       <tr>
         <th class="text-left">
           Emri
@@ -15,16 +15,23 @@
       </thead>
       <tbody>
       <tr v-for="res in restaurants"
-        :key="res.id">
-<!--          v-for="item in desserts"-->
-<!--          :key="item.name"-->
-            
+        :key="res._id">
         <td>{{res.name}}</td>
         <td>{{res.location[0].city}},{{res.location[0].street}}</td>
+        <v-card-actions>
+        <v-btn color="blue darken-1" dark v-on:click="editres(res._id)">Details</v-btn>
+        <v-btn color="blue darken-1" dark v-on:click="deleteres(res._id)">Delete</v-btn>
+        </v-card-actions>
       </tr>
       </tbody>
     </template>
   </v-simple-table>
+  <!-- <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
+        </v-card-actions> -->
+
 </template>
 
 <script>
@@ -42,6 +49,22 @@ name: "Restaurants",
         this.restaurants= res.data,
     ).catch();
   },
+  methods:{
+    editres(restaurant){
+      console.log(restaurant.id);
+      this.$router.push({
+        name:'EditRestaurant',
+        params:{id:restaurant.id}
+      })
+    },
+    deleteres(restaurant){
+      axios.delete('http://localhost:4000/restaurants/'+restaurant)
+      .then((res)=>{
+        console.log(res);
+        this.$router.go();
+      }).catch();
+    }
+  }
 }
 </script>
 
