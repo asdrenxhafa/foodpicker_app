09e03b31a6272e7ef060f48d53eb4b4cbfdb7494
Blerina Maxharraj <bm45555@ubt-uni.net>
2021-03-18 09:57:47 +0100

    front fix

--- front/src/components/FoodGallery/Gallery.vue
+++ front/src/components/FoodGallery/Gallery.vue
@@ -10,31 +10,31 @@
         <div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.3s">
           <a href="images/gallery-img1.jpg" data-lightbox-gallery="zenda-gallery"><img src="images/gallery-img1.jpg" alt="gallery img"></a>
           <div>
-            <h3>Tacos</h3>
+            <h3>Tacos</h3><br>
             <span>Ground meat / Onion / Shredded letuce</span>
           </div>
           <a href="images/gallery-img2.jpg" data-lightbox-gallery="zenda-gallery"><img src="images/gallery-img2.jpg" alt="gallery img"></a>
           <div>
-            <h3>PIZZA</h3>
+            <h3>PIZZA</h3><br>
             <span>Tomato / Mozzarella / Onions</span>
           </div>
         </div>
         <div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
           <a href="images/gallery-img3.jpg" data-lightbox-gallery="zenda-gallery"><img src="images/gallery-img3.jpg" alt="gallery img"></a>
           <div>
-            <h3>Sushi</h3>
+            <h3>Sushi</h3><br>
             <span>Soy souce / Rice / Avocado</span>
           </div>
         </div>
         <div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.9s">
           <a href="images/gallery-img4.jpg" data-lightbox-gallery="zenda-gallery"><img src="images/gallery-img4.jpg" alt="gallery img"></a>
           <div>
-            <h3>Risotto with salmon</h3>
+            <h3>Risotto with salmon</h3><br>
             <span>Salmon / Curry / Asparagus</span>
           </div>
           <a href="images/gallery-img5.jpg" data-lightbox-gallery="zenda-gallery"><img src="images/gallery-img5.jpg" alt="gallery img"></a>
           <div>
-            <h3>Spaghetti Carbonara</h3>
+            <h3>Spaghetti Carbonara</h3><br>
             <span>Prosciutto / Parmesan / Green</span>
           </div>
         </div>
