5ea08e62a56a64e5b0db836ac30be3af065cb981
Leutrim Ahmeti <trimmahmeti@outlook.com>
2021-03-28 18:56:01 +0200

    sending emails from contact page

--- front/src/components/Contact.vue
+++ front/src/components/Contact.vue
@@ -8,18 +8,18 @@
           <hr>
         </div>
         <div class="col-md-offset-1 col-md-10 col-sm-12 wow fadeIn" data-wow-delay="0.9s">
-          <form action="#" method="post">
+          <form @submit.prevent="sendEmail">
             <div class="col-md-6 col-sm-6">
-              <input name="name" type="text" class="form-control" id="name" placeholder="Name">
+              <input name="name" type="text" class="form-control" id="name" placeholder="Name" v-model="name">
             </div>
             <div class="col-md-6 col-sm-6">
-              <input name="email" type="email" class="form-control" id="email" placeholder="Email">
+              <input name="email" type="email" class="form-control" id="email" placeholder="Email" v-model="email">
             </div>
             <div class="col-md-12 col-sm-12">
-              <textarea name="message" rows="8" class="form-control" id="message" placeholder="Message"></textarea>
+              <textarea name="message" rows="8" class="form-control" id="message" placeholder="Message" v-model="message"></textarea>
             </div>
             <div class="col-md-offset-3 col-md-6 col-sm-offset-3 col-sm-6">
-              <input name="submit" type="submit" class="form-control" id="submit" value="make a reservation">
+              <input name="submit" type="submit" class="form-control" value="Send">
             </div>
           </form>
         </div>
@@ -30,8 +30,33 @@
 </template>
 
 <script>
+import emailjs from 'emailjs-com';
+
 export default {
-name: "Contact"
+name: "Contact",
+data(){
+  return {
+    name:'',
+    email:'',
+    message:''
+  }
+},
+methods:{
+  sendEmail(e){
+    try{
+      emailjs.sendForm('service_zo0r2ac','template_9b0lt2p',e.target,'user_sv4mq4lgK8UUlZvHgDCOs',{
+        name:this.name,
+        email:this.email,
+        message:this.message
+      })
+    } catch(error){
+      console.log(error);
+    }
+    this.name=''
+    this.email=''
+    this.message=''
+  },
+}
 }
 </script>
 
