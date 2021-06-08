6f955b3ace942bf0de622235d3252da72844b186
Asdren Xhafa <asdrenxhafa@gmail.com>
2021-06-08 11:56:02 +0200

    laravel init, models and controllers

---
+++ backend_laravel/database/migrations/2021_06_08_094645_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('user');
+            $table->string('oder_type_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('order_type_id')->references('id')->on('order_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
