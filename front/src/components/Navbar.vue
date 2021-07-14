e8b3a60b28cb56067b5313ae3bbe67baf46cc5fc
BlerinaMaxhrraj <blerina@tframe.de>
2021-07-14 11:58:03 +0200

    navbar fix

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -1,40 +1,40 @@
 <template>
-  <!-- navigation section -->
-  <section class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container">
-      <div class="navbar-header">
-        <a class="nav navbar-nav navbar-right" style="color:gray;text-align:right; font-size:20px; text-shadow: 1px 1px;">FOODPICKER</a>
+  <section>
+            
+<nav class = "navbar">
+      <div>
+        <a style="color:black; font-size:20px; text-align:justify;">FOODPICKER</a>
       </div>
-      <div class="collapse navbar-collapse">
+      <div>
      
-          <li><a href="#home" class="smoothScroll mr-4" style="color:gray;text-align:center; text-shadow: 1px 1px; font-size:15px;" ><b>HOME</b></a></li>
-          <li><a href="/Order" class="smoothScroll mr-4" style="color:gray;text-align:center;text-shadow: 1px 1px; font-size:15px;"><b>ORDER</b></a></li>
+          <li><a href="#home" act class="nav-link"><p>HOME</p></a></li>
+          <li><a href="/Order"  class="nav-link"><p><br>ORDER</p></a></li>
           <li>
             <template v-if="user.loggedIn">
               <div class="nav-item">{{user.data.displayName}}</div>
               
                   <li class="nav-item">
-                    <a class="nav-link" @click.prevent="signOut" style="color:gray;text-align:center; text-shadow: 1px 1px"><b>Sign out</b></a>
+                    <a class="nav-link" @click.prevent="signOut"><p>Sign out</p></a>
                   </li>
-              
+             
             </template>
             <template v-else>
-                 <div class="collapse navbar-collapse">
+                 <div>
                   
                   <li class="nav-item">
-                    <router-link to="/login" class="nav-link" style="color:gray;text-align:center;text-shadow: 1px 1px; font-size:15px;"><b>LOGIN</b></router-link>
+                    <router-link to="/login" class="nav-link"><p>LOGIN</p></router-link>
                   </li>
                   <li class="nav-item" >
-                    <router-link to="/register" class="nav-link" style="color:gray;text-align:center; text-shadow: 1px 1px;font-size:15px;"><b>REGISTER</b></router-link>
+                    <router-link to="/register" class="nav-link"><p>REGISTER</p></router-link>
                   </li>
                
                  </div>
             </template>
 
           </li>
-      
+       
       </div>
-    </div>
+      </nav>
   </section>
 </template>
 
